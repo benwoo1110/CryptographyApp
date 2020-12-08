16692e8e447bd92af05d309976f8bf3ce70ea8d1
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-08 15:49:24 +0800

    Add showing of avaliable ciphers in dropdown and some UI updates.

--- Cryptography.WinFormsApp/CryptoApp.Designer.cs
+++ Cryptography.WinFormsApp/CryptoApp.Designer.cs
@@ -29,96 +29,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.CipherAlgorithm = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.InputText = new System.Windows.Forms.RichTextBox();
+            this.KeyText = new System.Windows.Forms.RichTextBox();
+            this.OutputText = new System.Windows.Forms.RichTextBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.CipherMode = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // CipherAlgorithm
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(261, 25);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(388, 21);
-            this.comboBox1.TabIndex = 0;
+            this.CipherAlgorithm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.CipherAlgorithm.FormattingEnabled = true;
+            this.CipherAlgorithm.Location = new System.Drawing.Point(348, 31);
+            this.CipherAlgorithm.Margin = new System.Windows.Forms.Padding(4);
+            this.CipherAlgorithm.Name = "CipherAlgorithm";
+            this.CipherAlgorithm.Size = new System.Drawing.Size(516, 28);
+            this.CipherAlgorithm.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label1.Location = new System.Drawing.Point(66, 25);
+            this.label1.Location = new System.Drawing.Point(88, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(170, 21);
+            this.label1.Size = new System.Drawing.Size(227, 28);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cipher Algorithm:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // richTextBox1
+            // InputText
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(261, 98);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(388, 60);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
+            this.InputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.InputText.Location = new System.Drawing.Point(348, 122);
+            this.InputText.Margin = new System.Windows.Forms.Padding(4);
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(516, 73);
+            this.InputText.TabIndex = 3;
+            this.InputText.Text = "";
             // 
-            // richTextBox2
+            // KeyText
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(261, 173);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(388, 60);
-            this.richTextBox2.TabIndex = 4;
-            this.richTextBox2.Text = "";
+            this.KeyText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.KeyText.Location = new System.Drawing.Point(348, 213);
+            this.KeyText.Margin = new System.Windows.Forms.Padding(4);
+            this.KeyText.Name = "KeyText";
+            this.KeyText.Size = new System.Drawing.Size(516, 73);
+            this.KeyText.TabIndex = 4;
+            this.KeyText.Text = "";
             // 
-            // richTextBox3
+            // OutputText
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(261, 314);
-            this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(388, 106);
-            this.richTextBox3.TabIndex = 5;
-            this.richTextBox3.Text = "";
+            this.OutputText.Location = new System.Drawing.Point(348, 386);
+            this.OutputText.Margin = new System.Windows.Forms.Padding(4);
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(516, 130);
+            this.OutputText.TabIndex = 5;
+            this.OutputText.Text = "";
             // 
             // comboBox2
             // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(261, 61);
+            this.comboBox2.Location = new System.Drawing.Point(348, 75);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(388, 21);
+            this.comboBox2.Size = new System.Drawing.Size(516, 28);
             this.comboBox2.TabIndex = 6;
             // 
-            // button1
+            // Calculate
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.button1.Location = new System.Drawing.Point(529, 248);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 33);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Cacluate";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Calculate.Location = new System.Drawing.Point(705, 305);
+            this.Calculate.Margin = new System.Windows.Forms.Padding(4);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(160, 41);
+            this.Calculate.TabIndex = 7;
+            this.Calculate.Text = "Cacluate";
+            this.Calculate.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // CipherMode
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.button2.Location = new System.Drawing.Point(261, 248);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(242, 33);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Mode: Encypt";
-            this.button2.UseVisualStyleBackColor = true;
+            this.CipherMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.CipherMode.Location = new System.Drawing.Point(348, 305);
+            this.CipherMode.Margin = new System.Windows.Forms.Padding(4);
+            this.CipherMode.Name = "CipherMode";
+            this.CipherMode.Size = new System.Drawing.Size(323, 41);
+            this.CipherMode.TabIndex = 8;
+            this.CipherMode.Text = "Mode: Encrypt";
+            this.CipherMode.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label2.Location = new System.Drawing.Point(66, 61);
+            this.label2.Location = new System.Drawing.Point(88, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(170, 21);
+            this.label2.Size = new System.Drawing.Size(227, 28);
             this.label2.TabIndex = 9;
             this.label2.Text = "Text Type:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -126,9 +139,10 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label3.Location = new System.Drawing.Point(66, 98);
+            this.label3.Location = new System.Drawing.Point(88, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(170, 60);
+            this.label3.Size = new System.Drawing.Size(227, 74);
             this.label3.TabIndex = 10;
             this.label3.Text = "Plain Text:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -136,9 +150,10 @@
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label4.Location = new System.Drawing.Point(66, 173);
+            this.label4.Location = new System.Drawing.Point(88, 213);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(170, 60);
+            this.label4.Size = new System.Drawing.Size(227, 74);
             this.label4.TabIndex = 11;
             this.label4.Text = "Key:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -146,34 +161,44 @@
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label5.Location = new System.Drawing.Point(66, 314);
+            this.label5.Location = new System.Drawing.Point(88, 386);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(170, 106);
+            this.label5.Size = new System.Drawing.Size(227, 130);
             this.label5.TabIndex = 12;
             this.label5.Text = "Cipher Text:";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // CryptoApp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CipherMode);
+            this.Controls.Add(this.Calculate);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.richTextBox3);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.KeyText);
+            this.Controls.Add(this.InputText);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CipherAlgorithm);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CryptoApp";
-            this.Text = "Form1";
+            this.Text = "CryptoApp";
+            this.Load += new System.EventHandler(this.CryptoApp_Load);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.ComboBox CipherAlgorithm;
+        private System.Windows.Forms.Button CipherMode;
+        private System.Windows.Forms.RichTextBox InputText;
+        private System.Windows.Forms.RichTextBox KeyText;
+        private System.Windows.Forms.RichTextBox OutputText;
 
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -181,16 +206,9 @@
 
         private System.Windows.Forms.Label label2;
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox3;
 
         private System.Windows.Forms.Label label1;
-
-        private System.Windows.Forms.ComboBox comboBox1;
 
         #endregion
     }

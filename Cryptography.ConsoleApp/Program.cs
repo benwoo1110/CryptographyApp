7dcbe6f7d0f6acfe4e328f34acad999dd9f8f518
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-25 21:38:04 +0800

    Add examples.

--- Cryptography.ConsoleApp/Program.cs
+++ Cryptography.ConsoleApp/Program.cs
@@ -10,19 +10,43 @@ namespace Cryptography.ConsoleApp
         static void Main(string[] args)
         {
             CipherFactory cipherFactory = new CipherFactory();
-            
+
+            // Init cipher algorithms
             cipherFactory.RegisterCipher(new Blowfish());
             cipherFactory.RegisterCipher(new IDEA());
-            cipherFactory.RegisterCipher(new RC5());
-            cipherFactory.RegisterCipher(new Twofish());
-            cipherFactory.RegisterCipher(new SimpleTest());
 
-            cipherFactory.SelectCipher("IDEA");
+            // Set cipher algorithm to use
+            if (cipherFactory.SelectCipher("IDEA"))
+            {
+                Console.WriteLine("Selected cipher!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid cipher!");
+            }
 
-            cipherFactory.CipherMode = Mode.Encrypt;
-            CipherResult result = cipherFactory.RunCipher("05320a6414c819fa", "006400c8012c019001f4025802bc0320");
-            
-            Console.WriteLine(result);
+            // Set cipher mode -> Encrypt / Decrypt
+            cipherFactory.SetCipherMode("Encrypt");
+
+            // Text type:
+            // Decimal -> 1234567890
+            // Hex -> 1234567890ABCDE
+            // Binary -> 01
+            // Ascii -> A -> table to convert to hex
+            cipherFactory.SetTextType("Binary");
+
+            string input = "10101010002";
+            string key = "11101100";
+
+            // Run the cipher algorithm
+            CipherResult result = cipherFactory.RunCipher(input, key);
+
+            if (result.HasInvalidInputAndKey())
+            {
+                Console.WriteLine("Invalid input/key.");
+            }
+
+            Console.WriteLine(result.ToString());
         }
     }
 }

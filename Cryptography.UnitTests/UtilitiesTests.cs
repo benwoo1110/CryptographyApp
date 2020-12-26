619ebb817a2c595e3f750d945ab5758d29d66e63
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-26 23:33:00 +0800

    Add more tests for utilties and ciphers.

--- Cryptography.UnitTests/UtilitiesTests.cs
+++ Cryptography.UnitTests/UtilitiesTests.cs
@@ -14,19 +14,28 @@ namespace Cryptography.UnitTests
         }
 
         [Test]
-        public void ConvertToBigInt_Binary_ReturnsInteger()
+        [TestCase("000000", "0")]
+        [TestCase("111", "7")]
+        [TestCase("0001010", "10")]
+        [TestCase("error", "")]
+        public void ConvertToBigInt_Binary_ReturnsInteger(string binary, string expectedInteger)
         {
-            BigInteger? result = Utilities.ConvertToBigInt("1011", InputType.Binary);
+            BigInteger? result = Utilities.ConvertToBigInt(binary, InputType.Binary);
             
-            Assert.That(result.ToString(), Is.EqualTo("11"));
+            Assert.That(result.ToString(), Is.EqualTo(expectedInteger));
         }
         
         [Test]
-        public void ConvertToBigInt_Hex_ReturnsInteger()
+        [TestCase("2BD6459F82C5B300952C49104881FF48", "58269367470490382358246580833377976136")]
+        [TestCase("0ff", "255")]
+        [TestCase("0xab", "171")]
+        [TestCase("0x2Da", "730")]
+        [TestCase("error",  "")]
+        public void ConvertToBigInt_Hex_ReturnsInteger(string hex, string expectedInteger)
         {
-            BigInteger? result = Utilities.ConvertToBigInt("2BD6459F82C5B300952C49104881FF48", InputType.Hex);
+            BigInteger? result = Utilities.ConvertToBigInt(hex, InputType.Hex);
             
-            Assert.That(result.ToString(), Is.EqualTo("58269367470490382358246580833377976136"));
+            Assert.That(result.ToString(), Is.EqualTo(expectedInteger));
         }
         
         [Test]
@@ -46,7 +55,7 @@ namespace Cryptography.UnitTests
         }
 
         [Test]
-        public void ConvertToString_WhenCalled_ReturnBinaryString()
+        public void ConvertToString_Binary_ReturnBinaryString()
         {
             string result = Utilities.ConvertToString(BigInteger.Parse("5552913087"), InputType.Binary);
 
@@ -54,7 +63,7 @@ namespace Cryptography.UnitTests
         }
         
         [Test]
-        public void ConvertToString_WhenCalled_ReturnHexString()
+        public void ConvertToString_Hex_ReturnHexString()
         {
             string result = Utilities.ConvertToString(BigInteger.Parse("58269367470490382358246580833377976136"), InputType.Hex);
             
@@ -62,7 +71,7 @@ namespace Cryptography.UnitTests
         }
         
         [Test]
-        public void ConvertToString_WhenCalled_ReturnDecimalString()
+        public void ConvertToString_Decimal_ReturnDecimalString()
         {
             string result = Utilities.ConvertToString(BigInteger.Parse("189732465783642956328495689237465234654"), InputType.Decimal);
             
@@ -70,13 +79,13 @@ namespace Cryptography.UnitTests
         }
         
         [Test]
-        public void ConvertToString_WhenCalled_ReturnAsciiString()
+        public void ConvertToString_Ascii_ReturnAsciiString()
         {
             string result = Utilities.ConvertToString(BigInteger.Parse("87521618088882533792115812"), InputType.Ascii);
             
             Assert.That(result, Is.EqualTo("Hello World"));
         }
-        
+
         [Test]
         [TestCase("15", 4)]
         [TestCase("5552913087", 33)]
@@ -85,6 +94,30 @@ namespace Cryptography.UnitTests
             int result = Utilities.NumberOfBits(BigInteger.Parse(number));
             
             Assert.That(result, Is.EqualTo(bitLength));
+        }
+
+        [Test]
+        [TestCase(true, ConvertResult.Valid)]
+        [TestCase(false, ConvertResult.Invalid)]
+        public void ValidationResult_WhenCalled_ReturnConvertResult(bool state, ConvertResult expectedResult)
+        {
+            ConvertResult result = Utilities.ValidationResult(state);
+            
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("word", "Word")]
+        [TestCase("CAPS", "Caps")]
+        [TestCase("tEST", "Test")]
+        [TestCase("aMaZiNg", "Amazing")]
+        [TestCase("", "")]
+        [TestCase(null, "")]
+        public void Capitalise_WhenCalled_ReturnCapitalisedWord(string word, string expectedResult)
+        {
+            string result = Utilities.Capitalise(word);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }

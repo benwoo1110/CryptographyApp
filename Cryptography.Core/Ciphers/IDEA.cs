bb3871e1fe1f627fc0a19b4b5812e6e44c650ea8
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-28 16:53:47 +0800

    Add IDEA cipher decryption algorithm.

--- Cryptography.Core/Ciphers/IDEA.cs
+++ Cryptography.Core/Ciphers/IDEA.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace Cryptography.Core.Ciphers
@@ -80,29 +82,7 @@ namespace Cryptography.Core.Ciphers
         {
             return (subText[0] << 48) | (subText[1] << 32) | (subText[2] << 16) | subText[3];
         }
-
-        public override BigInteger Encrypt(BigInteger plaintext, BigInteger key)
-        {
-            List<BigInteger> subText = GenerateSubText(plaintext);
-            List<BigInteger> subKeys = GenerateSubKeys(key);
-
-            for (int i = 0; i < 8; i++)
-            {
-                subText = EncryptionRound(subText, subKeys.GetRange(i * 6, 6));
-                
-                if (i != 7)
-                {
-                    BigInteger temp = subText[1];
-                    subText[1] = subText[2];
-                    subText[2] = temp;
-                }
-            }
-            
-            subText = EncryptionLastRound(subText, subKeys.GetRange(48, 4));
-
-            return CombineText(subText);
-        }
-
+        
         private List<BigInteger> EncryptionRound(List<BigInteger> subText, List<BigInteger> subKeys)
         {
             BigInteger step1 = MultiplyModulo(subText[0], subKeys[0]);
@@ -133,9 +113,115 @@ namespace Cryptography.Core.Ciphers
             return new List<BigInteger>{ step1, step2, step3, step4 };
         }
 
+        public override BigInteger Encrypt(BigInteger plaintext, BigInteger key)
+        {
+            List<BigInteger> subText = GenerateSubText(plaintext);
+            List<BigInteger> subKeys = GenerateSubKeys(key);
+
+            for (int i = 0; i < 8; i++)
+            {
+                subText = EncryptionRound(subText, subKeys.GetRange(i * 6, 6));
+                
+                if (i != 7)
+                {
+                    BigInteger temp = subText[1];
+                    subText[1] = subText[2];
+                    subText[2] = temp;
+                }
+            }
+            
+            subText = EncryptionLastRound(subText, subKeys.GetRange(48, 4));
+
+            return CombineText(subText);
+        }
+
+        private BigInteger AddInverse(BigInteger x)
+        {
+            return (0x10000 - x) & 0xFFFF;
+        }
+
+        private BigInteger MultiplyInverse(BigInteger x)
+        {
+            if (x <= 1) {
+                // 0 and 1 are their own inverses
+                return x;
+            }
+            try {
+                BigInteger y = 0x10001;
+                BigInteger t0 = 1;
+                BigInteger t1 = 0;
+                while (true) {
+                    t1 += y / x * t0;
+                    y %= x;
+                    if (y == 1) {
+                        return (1 - t1) & 0xffff;
+                    }
+                    t0 += x / y * t1;
+                    x %= y;
+                    if (x == 1) {
+                        return t0;
+                    }
+                }
+            } catch (ArithmeticException e) {
+                return 0;
+            }
+        }
+
+        private List<BigInteger> InverseKeyGeneration(List<BigInteger> subkey)
+        {
+            BigInteger[] invSubKey = new BigInteger[subkey.Count];
+            int p = 0;
+            int i = 8 * 6;
+            
+            // For the final output transformation (round 9)
+            invSubKey[i]     = MultiplyInverse(subkey[p++]);    // 48 <- 0
+            invSubKey[i + 1] = AddInverse(subkey[p++]);         // 49 <- 1
+            invSubKey[i + 2] = AddInverse(subkey[p++]);         // 50 <- 2
+            invSubKey[i + 3] = MultiplyInverse(subkey[p++]);    // 51 <- 3
+            
+            // From round 8 to 2
+            for (int r = 8 - 1; r > 0; r--) {
+                i = r * 6;
+                invSubKey[i + 4] = subkey[p++];                    // 46 <- 4 ...
+                invSubKey[i + 5] = subkey[p++];                    // 47 <- 5 ...
+                invSubKey[i]     = MultiplyInverse(subkey[p++]);   // 42 <- 6 ...
+                invSubKey[i + 2] = AddInverse(subkey[p++]);        // 44 <- 7 ...
+                invSubKey[i + 1] = AddInverse(subkey[p++]);        // 43 <- 8 ...
+                invSubKey[i + 3] = MultiplyInverse(subkey[p++]);   // 45 <- 9 ...
+            }
+            
+            // Round 1
+            invSubKey[4] = subkey[p++];                     // 4 <- 46
+            invSubKey[5] = subkey[p++];                     // 5 <- 47
+            invSubKey[0] = MultiplyInverse(subkey[p++]);    // 0 <- 48
+            invSubKey[1] = AddInverse(subkey[p++]);         // 1 <- 49
+            invSubKey[2] = AddInverse(subkey[p++]);         // 2 <- 50
+            invSubKey[3] = MultiplyInverse(subkey[p]);      // 3 <- 51
+            
+            return invSubKey.ToList();
+        }
+
         public override BigInteger Decrypt(BigInteger ciphertext, BigInteger key)
         {
-            throw new System.NotImplementedException();
+            List<BigInteger> subText = GenerateSubText(ciphertext);
+            List<BigInteger> subKeys = InverseKeyGeneration(GenerateSubKeys(key));
+            
+            for (int i = 0; i < 8; i++)
+            {
+                subText = EncryptionRound(subText, subKeys.GetRange(i * 6, 6));
+                
+                if (i != 7)
+                {
+                    BigInteger temp = subText[1];
+                    subText[1] = subText[2];
+                    subText[2] = temp;
+                }
+            }
+            
+            subText = EncryptionLastRound(subText, subKeys.GetRange(48, 4));
+
+            return CombineText(subText);
         }
     }
 }
+// Reference link: https://github.com/davidmigloz/IDEA-cipher
